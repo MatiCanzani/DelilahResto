515c7aaa618a1859aa53833a4ddb6c8201e4f29b
Mati Canzani <matiascanzani@gmail.com>
2020-07-17 17:53:02 -0300

    cambio conig db

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const config = {
     username : 'bf8e43b66ab68c',
     password : '52791f02',
     dataBase : 'heroku_87e325f58fecdec',
-    host: 'us-cdbr-east-02.cleardb.com',
-    port : '3306',
+    host: 'us-cdbr-east-02.cleardb.com'
 }
-
+//mysql://bf8e43b66ab68c:52791f02@us-cdbr-east-02.cleardb.com/heroku_87e325f58fecdec
 module.exports = config
