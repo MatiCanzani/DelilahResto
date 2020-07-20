f0c4ca5c60dc5dcce496c44deb3dd3498b8ee2f7
Mati Canzani <matiascanzani@gmail.com>
2020-07-20 16:21:05 -0300

    cambios en los permisos

--- api/database/db.js
+++ api/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const dataBase = require('../../config');
 const sequelize  = new Sequelize(`mysql://${dataBase.username}:${dataBase.password}@${dataBase.host}:${dataBase.port}/${dataBase.dataBase}`)
-
+//const sequelize  = new Sequelize(`mysql://${dataBase.username}@${dataBase.host}/${dataBase.dataBase}`)
 module.exports = sequelize;
 
