6db3d122e6db98713d8c50dc3e0706ed9a38be5c
Mati Canzani <matiascanzani@gmail.com>
2020-07-20 17:11:37 -0300

    mensaje login

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-const config = {
-    username : 'root',
-    password : 'root',
-    dataBase : 'Delilah_Resto',
-    host: 'localhost',
-    port: 8889
-}
-
 // const config = {
-//     username : 'bf8e43b66ab68c',
-//     password : '52791f02',
-//     dataBase : 'heroku_87e325f58fecdec',
-//     host: 'us-cdbr-east-02.cleardb.com'
+//     username : 'root',
+//     password : 'root',
+//     dataBase : 'Delilah_Resto',
+//     host: 'localhost',
+//     port: 8889
 // }
+
+const config = {
+    username : 'bf8e43b66ab68c',
+    password : '52791f02',
+    dataBase : 'heroku_87e325f58fecdec',
+    host: 'us-cdbr-east-02.cleardb.com'
+}
 module.exports = config
